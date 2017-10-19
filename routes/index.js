57b1ddb7304cd350588a3981a03f66e5326742ef
Colm Begley <pair+cbegl@allstate.com>
2017-10-19 14:09:56 +0100

    added petBot access

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OurBOT' });
+  res.render('index', { title: 'petBot' });
 });
 
 module.exports = router;
